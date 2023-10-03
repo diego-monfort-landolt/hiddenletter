ea3272f90e7bbbd289b7acc5f174e68b314e053c
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-10-03 17:40:25 +0200

    delet spaces in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,56 +16,35 @@ function App() {
       }, 100)
       return () => clearInterval(interval)
     }
-
   }, [gamesToWin, timer]);
 
-
-
   function handleClick(row, col) {
-
     if (board[row][col].isHidden) {
 
       setGamesToWin(gamesToWin - 1);
-
       setTimeout(() => {
         setBoard(CreateBoard())
       }, 500);
       setBoard(CreateBoard());
     }
   }
-
-
   return (
     <>
       <div className='app'>
         <h1><span className='span-title'>D</span>iego<span className='span-title'>D</span>ev</h1>
         {gamesToWin > 0 && <h2>Time: {miliseconds}</h2>}
-
-
-
-
         {gamesToWin === 0 ? (
           <h2>¡ Congratulations ! Your time was: {miliseconds}</h2>
         ) : (
           <p>Games To Win: {gamesToWin}</p>
         )}
-
-
         {gamesToWin > 0 && <div> {board.map((row, rowIdx) => (
           <div key={rowIdx} className='row' >{row.map((letter, letterIdx) => (
             <Cell key={letterIdx} handleClick={handleClick} {...letter} />
           ))}</div>
-
         ))}
-
         </div>}
-
-
-
-
-
       </div>
-
     </>
   )
 }
